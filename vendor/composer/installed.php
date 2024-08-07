658aa94ad1258d22d39e88cfd12489f626170e19
Aaron Ainsworth <105944277+II-Wolf-II@users.noreply.github.com>
2024-08-06 21:49:58 -0400

    Composer install

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b493a23060a1df9e0cf84bb94072a35e69d1275f',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b493a23060a1df9e0cf84bb94072a35e69d1275f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
